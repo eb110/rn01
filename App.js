2de74cdec2100a861bcfda9b7a556b612f674600
eb110 <wfigura@op.pl>
2021-10-04 21:46:25 +0100

    AppNavigator navigation

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './app/navigation/AuthNavigator';
+import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
 
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
+
 }
