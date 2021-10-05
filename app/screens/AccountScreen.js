19dce754be928b55384a99b3c162213cc3b9e797
eb110 <wfigura@op.pl>
2021-10-05 10:00:34 +0100

    AccountNavigator navigation

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -13,18 +13,20 @@ const menuItems = [
         icon: {
             name: 'format-list-bulleted',
             backgroundColor: colors.primary,
-        }
+        },
+        targetScreen: 'MyListings',
     },
     {
         title: 'My Messages',
         icon: {
             name: 'email',
             backgroundColor: colors.secondary,
-        }
+        },
+        tergetScreen: 'Messages',
     },
 ]
 
-function AccountScreen(props) {
+function AccountScreen({ navigation }) {
     return (
         <Screen style={styles.screen}>
             <View style={styles.container}>
@@ -45,6 +47,7 @@ function AccountScreen(props) {
                                 name={item.icon.name}
                                 backgroundColor={item.icon.backgroundColor}
                             />}
+                            onPress={() => navigation.navigate(item.tergetScreen)}
                         />
                     }
                     ItemSeparatorComponent={ListItemSeparator}
